de2d0aaa2c8e36121ae9b5d1790e7b2262e5429e
Sandra Szenti <alexandraszenti@gmail.com>
2014-04-04 18:05:15 -0400

    contact site ready

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
+var map;
+function mapLoad () {
+	var mapOptions = {
+	    zoom: 14,
+	    center: new google.maps.LatLng(47.469935, 19.083579)
+	  };
+	  map = new google.maps.Map(document.getElementById('map-canvas'),
+	      mapOptions);
+}
+
 function initialize() {
-	
+	mapLoad();
 }
 
 $(document).ready(initialize);
