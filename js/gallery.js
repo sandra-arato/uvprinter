2fda2e8f3656698b4a0f2ad906a150c1533eb90d
Sandra Szenti <alexandraszenti@gmail.com>
2014-04-25 20:51:04 +0200

    JS span arrow bugfix

--- js/gallery.js
+++ js/gallery.js
@@ -18,7 +18,10 @@ function initialize() {
 	// reload large photo div content with clicked photo
 	$("div.photo img").click(function () {
 		var indexOfPhoto = $(this).attr("src").split("/")[6];
-		$("#large-photo").html("<img src=http://lorempixel.com/800/600/nightlife/" + indexOfPhoto + " />")
+		$("#large-photo").html("<img src=http://lorempixel.com/800/600/nightlife/" + indexOfPhoto + " /> \
+			<span id='back' class='glyphicon glyphicon-chevron-left'></span> \
+			<span id='forward' class='glyphicon glyphicon-chevron-right'></span> \
+			")
 	})
 
 
