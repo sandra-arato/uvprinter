fe646c33c07c68dd16b9b673bc201febee8e9a18
Sandra Szenti <alexandraszenti@gmail.com>
2014-04-25 19:35:44 +0200

    auto height for large photo

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,3 @@
-function clickPhoto(photo) {
-	console.log("click!");
-	console.log($(this));
-}
-
-
 
 function initialize() {
 
@@ -14,13 +8,9 @@ function initialize() {
 	var html = new EJS({url: 'js/photos.ejs'}).render(dataGallery);
 	$(html).appendTo($("#gallery div"));
 
-	
 	$("div.photo img").click(function () {
-		console.log("click!!");
 		var indexOfPhoto = $(this).attr("src").split("/")[6];
-		console.log("this is " + indexOfPhoto);
 		$("#large-photo").html("<img src=http://lorempixel.com/800/600/nightlife/" + indexOfPhoto + " />")
-
 	})
 
 }
