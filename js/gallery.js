a203e0a492b73ef431abbd3b46dac7267311ad82
Sandra Szenti <alexandraszenti@gmail.com>
2014-04-25 19:44:11 +0200

    get and set large photo div height with JS

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,7 @@
 
 function initialize() {
 
+	// load gallery images with embedded JS
 	var dataGallery	= {
 		title: "Kepek",
 		supplies: ["domestos", "mososzer", "valami mas"]
@@ -8,10 +9,19 @@ function initialize() {
 	var html = new EJS({url: 'js/photos.ejs'}).render(dataGallery);
 	$(html).appendTo($("#gallery div"));
 
+	// set large photo div height 
+	$("#large-photo").css("height", $("#large-photo").height() + "px");
+	$(window).resize(function () {
+		$("#large-photo").css("height", $("#large-photo").height() + "px");		
+	})
+
+	// reload large photo div content with clicked photo
 	$("div.photo img").click(function () {
 		var indexOfPhoto = $(this).attr("src").split("/")[6];
 		$("#large-photo").html("<img src=http://lorempixel.com/800/600/nightlife/" + indexOfPhoto + " />")
 	})
+
+
 
 }
 
