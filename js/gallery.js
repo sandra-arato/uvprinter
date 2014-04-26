b612d1645cefee3c3ef8d784e42c04961863ba1a
Sandra Szenti <alexandraszenti@gmail.com>
2014-04-26 17:33:25 +0200

    arrow navigation and thumbnail nav simplified

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,4 @@
-
-function initialize() {
-
+function renderThumbnails () {
 	// load gallery images with embedded JS
 	var dataGallery	= {
 		title: "Kepek",
@@ -8,23 +6,53 @@ function initialize() {
 	}
 	var html = new EJS({url: 'js/photos.ejs'}).render(dataGallery);
 	$(html).appendTo($("#gallery"));
+}
+
+function arrowNav(direction) {
+	console.log("click direction", direction.id);
+	var currentPhotoIndex = $("#large-photo img").attr("src").split("/")[6];
+	var indexOfPhoto;
+	if (direction.id === "back") {
+		indexOfPhoto = parseInt(currentPhotoIndex) - 1;
+		console.log("Going back from " + currentPhotoIndex + " to " + indexOfPhoto);
+		if (indexOfPhoto < 1) {
+			indexOfPhoto = 10;
+		}
+	}
+	else if (direction.id === "forward") {
+		indexOfPhoto = parseInt(currentPhotoIndex) + 1;
+		console.log("Going forward from " + currentPhotoIndex + " to " + indexOfPhoto);
+		if (indexOfPhoto > 10) {
+			indexOfPhoto = 1;
+		}
+	}
+	else {
+		console.log("error");
+		return;
+	}
+
+	$("#large-photo img").attr("src", "http://lorempixel.com/800/600/nightlife/" + indexOfPhoto );
+}
+
+function thumbnailClick(photo) {
+	// reload large photo div content with clicked photo
+	var indexOfPhoto = $(photo).attr("src").split("/")[6];
+	$("#large-photo img").attr("src", "http://lorempixel.com/800/600/nightlife/" + indexOfPhoto );
+}
+
+
+function initialize() {
 
 	// set large photo div height 
 	$("#large-photo").css("height", $("#large-photo").height() + "px");
 	$(window).resize(function () {
 		$("#large-photo").css("height", $("#large-photo img").height() + "px");		
-	})
+	});
 
-	// reload large photo div content with clicked photo
-	$("div.photo img").click(function () {
-		var indexOfPhoto = $(this).attr("src").split("/")[6];
-		$("#large-photo").html("<img src=http://lorempixel.com/800/600/nightlife/" + indexOfPhoto + " /> \
-			<span id='back' class='glyphicon glyphicon-chevron-left'></span> \
-			<span id='forward' class='glyphicon glyphicon-chevron-right'></span> \
-			")
-	})
-
-
+	$("#large-photo span").click(function () { arrowNav(this); });
+	$("div.photo img").click(function () { thumbnailClick(this); });
+	renderThumbnails();
+	photoNavigation();
 
 }
 
