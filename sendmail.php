74c10d1fab93dd74c4aba1005900ee85487224ba
Sandra Szenti <alexandraszenti@gmail.com>
2014-05-11 13:27:08 +0200

    sendmail php w PHPMailer added

---
+++ sendmail.php
@@ -0,0 +1,70 @@
+<?php
+
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+date_default_timezone_set('Etc/UTC');
+
+require 'PHPMailer/PHPMailerAutoload.php';
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer();
+
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "uprinter.hu@gmail.com";
+
+//Password to use for SMTP authentication
+$mail->Password = "RolandLEF20";
+
+//Set who the message is to be sent from
+$mail->setFrom('uvprinter.hu@gmail.com', 'Roland LEF20');
+
+//Set an alternative reply-to address
+$mail->addReplyTo('sandraszenti@gmail.com', 'alexandra sandra');
+
+//Set who the message is to be sent to
+$mail->addAddress('alexandraszenti@gmail.com', 'John Doe');
+
+//Set the subject line
+$mail->Subject = 'PHPMailer GMail SMTP test';
+
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->Body    = 'This is the HTML test message body <b>in bold!</b>';
+
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+
+//Attach an image file
+// $mail->addAttachment('images/phpmailer_mini.png');
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
+?>
